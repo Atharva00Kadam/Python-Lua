b0be064cbba8aa2eedfc1b989d270c97cb9c2750
Atharva Kadam <165936212+Atharva00Kadam@users.noreply.github.com>
2024-10-14 18:49:30 +0530

    Update Task-Spot-code.py

--- Task-Spot-code.py
+++ Task-Spot-code.py
@@ -1,15 +1,3 @@
-def load_tasks():
-    """Load tasks from a file."""
-    if os.path.exists(TASK_FILE):
-        with open(TASK_FILE, 'rb') as file:
-            return pickle.load(file)
-    return []
-
-def save_tasks(tasks):
-    """Save tasks to a file."""
-    with open(TASK_FILE, 'wb') as file:
-        pickle.dump(tasks, file)
-
 def display_tasks(tasks):
     """Display all tasks."""
     if not tasks:
@@ -67,3 +55,59 @@ def main():
 
 if __name__ == "__main__":
     main()
+###################################################################################
+tasks = []
+
+def addTask():
+    task = input("PLease enter a task!: ")
+    tasks.append(task)
+    print(f"Task '{task}' added to the list. )
+          
+def listTasks():
+    if not tasks:
+        print("There are no tasks there currently!")
+    else:
+        print("Current Tasks:")
+        for index, task in enumerate(tasks):
+            print(f"Task #{index}. {task}"))
+            
+        ## Task #1 take out the groceries!
+        
+def deleteTask():
+    listTasks()
+    try:
+        taskToDelete - int(input("Enter the # to delete"))
+        if taskToDelete < len(tasks):
+            tasks.pop(taksToDelete)
+            print(f"Task '{taskToDelete}' has been removed.)
+            else
+        
+    except:
+        print("Invalid statement")
+        
+if __name__ == "__main__":
+    # Creating a loop to run the app
+print(" Welcome to TaskiD0!- the to-do list app :) ")
+while True:
+    print(\n)
+    print("Please select the following options: ")
+    print("-------------------------------------")
+    print("1. Add a new task")
+    print("2. Delete a task")
+    print("3. List a task")
+    print("4. Quit")
+    
+choice = input("Enter your choice: ")
+
+if(choice == "1"):
+ addTask()   
+elif(choice == "2"):
+ deleteTask()
+elif(choice == "3"):
+ listTask()
+elif(choice == "4"):
+    break
+else:
+    print("Invalid input. Please try again.")
+    
+    print("GoodBye! ✋✋🤟")

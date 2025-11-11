357da65675cb0f9ac34fedf336a7afcdb7f2726e
Atharva Kadam <165936212+Atharva00Kadam@users.noreply.github.com>
2025-11-11 17:35:21 -0500

    Update read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -1,1 +1,23 @@
+# Author   : Atharva Kadam
+# Email    : aakadam@umass.edu
+# Spire ID : 35090900
+
+def read_csv(fname):
+  try:
+    f = open("students.csv", 'r')
+    the_dict_we_use = {}
+    dict_encapsulated_list = []
+    for i in f.readlines():
+      if i == "Students""
+        if len(dict_encapsulated_list) == len(Students in f):
+          
+          
+  except:
+    if f.read() == "":
+      return None
+    if not os.path.exists({fname}):
+      print(f"Error occurred when opening {fname} to read")
+    raise IsADirectoryError(f'Error occurred when opening {fname} to read')
+      
+  
 
